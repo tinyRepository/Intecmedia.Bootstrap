afa9a877989c08008a9a65e48863e69ab70e3c3d
dkrnl <dkrnl@yandex.ru>
2014-06-04 17:37:16 +0800

    file protocol fix

--- js/application.js
+++ js/application.js
@@ -1,4 +1,19 @@
 /*! Intecmedia.Bootstrap  | (c) 2014 Intecmedia. | license public domain */
+if (/^(file|chrome(-extension)?|resource|qrc|app):/.test(location.protocol)) {
+    jQuery.ajax({
+        cache: true,
+        url: "js/less.js",
+        dataType: "script",
+        success: function() {
+            less.env = 'development';
+            jQuery('link.less').each(function() {
+                less.sheets.push(this);
+            });
+            less.refresh();
+        }
+    });
+}
+
 jQuery(function($) {
     var wnd = $(window);
     var doc = $(document);
