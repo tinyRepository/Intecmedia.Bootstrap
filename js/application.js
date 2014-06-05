75ce52c4de2d73f5997650ccbebde501f906ba82
dkrnl <dkrnl@yandex.ru>
2014-06-05 11:21:49 +0800

    fix file protocol

--- js/application.js
+++ js/application.js
@@ -1,21 +1,21 @@
 /*! Intecmedia.Bootstrap  | (c) 2014 Intecmedia. | license public domain */
 (function() {
-    if (location.protocol !== "file:") return;
+    if (window.location.protocol !== "file:") return;
     var links = document.getElementsByTagName("link");
     var head = document.getElementsByTagName("head")[0];
     var script = document.createElement("script");
-    script.src = "js/less.js";
     script.type = "text/javascript";
     script.onload = function() {
         window.less.env = "development";
         for (var i = 0; i < links.length; i++) {
-            if (links[i].href.match(/.less$/)) {
+            if (links[i].rel.match(/stylesheet/) && links[i].href.match(/.less$/)) {
                 window.less.sheets.push(links[i]);
                 head.removeChild(links[i]);
             }
         }
         window.less.refresh();
     };
+    script.src = "js/less.js";
     head.appendChild(script);
 })();
 
