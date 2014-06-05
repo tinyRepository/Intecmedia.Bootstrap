978c8f55716d6a190f254c537297a2c87606e7bd
dkrnl <dkrnl@yandex.ru>
2014-06-05 10:00:34 +0800

    refactoring file protocol

--- js/application.js
+++ js/application.js
@@ -1,24 +1,22 @@
 /*! Intecmedia.Bootstrap  | (c) 2014 Intecmedia. | license public domain */
-if (/^(file|chrome(-extension)?|resource|qrc|app):/.test(location.protocol)) {
-    (function() {
-        var links = document.getElementsByTagName("link");
-        var head = document.getElementsByTagName("head")[0];
-        var script = document.createElement("script");
-        script.src = "js/less.js";
-        script.type = "text/javascript";
-        script.onload = function() {
-            window.less.env = "development";
-            for (var i = 0; i < links.length; i++) {
-                if (links[i].href.match(/.less$/)) {
-                    window.less.sheets.push(links[i]);
-                    head.removeChild(links[i]);
-                }
+if (location.protocol == "file:") (function() {
+    var links = document.getElementsByTagName("link");
+    var head = document.getElementsByTagName("head")[0];
+    var script = document.createElement("script");
+    script.src = "js/less.js";
+    script.type = "text/javascript";
+    script.onload = function() {
+        window.less.env = "development";
+        for (var i = 0; i < links.length; i++) {
+            if (links[i].href.match(/.less$/)) {
+                window.less.sheets.push(links[i]);
+                head.removeChild(links[i]);
             }
-            window.less.refresh();
-        };
-        head.appendChild(script);
-    })();
-}
+        }
+        window.less.refresh();
+    };
+    head.appendChild(script);
+})();
 
 jQuery(function($) {
     var wnd = $(window);
