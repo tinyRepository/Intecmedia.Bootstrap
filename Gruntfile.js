5cd6ed4ea90a05b88fcb168891282c3bfda5c631
dkrnl <dkrnl@yandex.ru>
2015-07-15 11:33:35 +0700

    Grunt less task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,10 @@
 module.exports = function (grunt) {
     "use strict";
 
-    grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-contrib-less");
     grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-lesslint");
 
     grunt.initConfig({
-        jshint: {
-            files: ["Gruntfile.js", "js/application.js"],
-            options: {
-                globals: {
-                    jQuery: true,
-                    console: true,
-                    module: true,
-                    document: true
-                }
-            }
-        },
         less: {
             production: {
                 options: {
@@ -40,12 +27,8 @@ module.exports = function (grunt) {
             files: "css/*.less",
             tasks: ["less"]
         },
-        lesslint: {
-            src: ["css/bootstrap.less", "css/style.less"]
-        }
     });
 
-    grunt.registerTask("lint", ["jshint", "lesslint"]);
     grunt.registerTask("less", ["less"]);
     grunt.registerTask("watch", ["watch"]);
 };
